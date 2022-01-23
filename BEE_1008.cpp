ac6d06a2dc53bfaf8a98966ef4dc142e8427dc6c
Md. Nayim Islam <77149380+MdNayimIslam@users.noreply.github.com>
2022-01-24 00:27:15 +0600

    Add files via upload

---
+++ BEE_1008.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N, T;
+    float A;
+    cin >> N >> T >> A;
+    cout << fixed << setprecision(2) << "NUMBER = " << N << endl
+         << "SALARY = U$ " << T * A << endl;
+    return 0;
+}
