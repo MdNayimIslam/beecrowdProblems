69e88831aee81e455637abe006ff80101c8904d2
Md. Nayim Islam <77149380+MdNayimIslam@users.noreply.github.com>
2022-01-23 14:33:54 +0600

    Add files via upload

---
+++ BEE_1002.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double A,n=3.14159,R;
+    cin>>R;
+    A=n* pow(R,2);
+    cout<<fixed<<setprecision(4)<<"A="<<A<<endl;
+    return 0;
+}
