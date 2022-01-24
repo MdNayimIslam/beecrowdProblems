15ae093ebe7b0b9fbd90295eb5c842a008a1c429
Md. Nayim Islam <77149380+MdNayimIslam@users.noreply.github.com>
2022-01-25 00:28:44 +0600

    Add files via upload

---
+++ BEE_1014.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int A;
+    double B;
+    cin >> A >> B;
+    cout << fixed << setprecision(3) << (A / B) << " km/l" << endl;
+    return 0;
+}
