6f0008ed37138793d66ce1a9719fc1d219f4658a
Md. Nayim Islam <77149380+MdNayimIslam@users.noreply.github.com>
2022-01-26 00:16:24 +0600

    Add files via upload

---
+++ BEE_1017.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    double C;
+    cin >> A >> B;
+    C = (double(A) * B) / 12;
+    cout << fixed << setprecision(3) << C << endl;
+    return 0;
+}
