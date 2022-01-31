68f7d8f95194aef1f5d2e799b3d1409d1cd692b5
Md. Nayim Islam <77149380+MdNayimIslam@users.noreply.github.com>
2022-01-31 11:12:21 +0600

    Add files via upload

---
+++ BEE_1098.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    for(float i=0; i<=2.2; i+=.2)
+    {
+        for(float j=1; j<=3; j++)
+        {
+            cout<<"I="<<i<<" J="<<j+i<<endl;
+        }
+    }
+ 
+    return 0;
+}
