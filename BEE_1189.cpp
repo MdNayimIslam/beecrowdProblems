6f32de482087295814bef149bb3e98fff27639f8
Md. Nayim Islam <77149380+MdNayimIslam@users.noreply.github.com>
2022-02-05 17:12:10 +0600

    Add files via upload

---
+++ BEE_1189.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    double M[12][12], sum = 0.0;
+    char C;
+    cin >> C;
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            cin >> M[i][j];
+        }
+    }
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            if (j < i && i < 6)
+            {
+                sum += M[i][j];
+            }
+            else if (j < (11-i)  && i > 5)
+            {
+                sum += M[i][j];
+            }
+        }
+    }
+    if (C == 'S')
+    {
+        cout << fixed << setprecision(1) << sum << endl;
+    }
+    else if (C == 'M')
+    {
+        cout << fixed << setprecision(1) << sum / 30.00 << endl;
+    }
+    return 0;
+}
---
+++ BEE_1190.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    double M[12][12], sum = 0.0;
+    char C;
+    cin >> C;
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            cin >> M[i][j];
+            if ((j > 11 - i) && i < j)
+            {
+                sum += M[i][j];
+            }
+        }
+    }
+    if (C == 'S')
+    {
+        cout << fixed << setprecision(1) << sum << endl;
+    }
+    else if (C == 'M')
+    {
+        cout << fixed << setprecision(1) << sum / 30.00 << endl;
+    }
+    return 0;
+}
