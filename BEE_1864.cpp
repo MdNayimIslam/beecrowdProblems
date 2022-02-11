641f42a7d21455339cc97f2ac7ad2bbbf34e87c9
Md. Nayim Islam <77149380+MdNayimIslam@users.noreply.github.com>
2022-02-11 23:35:58 +0600

    Add files via upload

---
+++ BEE_1864.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    string S = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    for (int i = 0; i < n; i++)
+    {
+        cout << S[i];
+    }
+    cout << endl;
+
+    return 0;
+}
